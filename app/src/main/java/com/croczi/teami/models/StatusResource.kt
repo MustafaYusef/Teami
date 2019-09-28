81773c463f5f4df1f70a9b65c594071269624634
Mmdakhil <mmdakhil@earthlink.iq>
2019-09-28 08:42:11 +0300

    added missing files

---
+++ app/src/main/java/com/croczi/teami/models/StatusResource.kt
@@ -0,0 +1,32 @@
+package com.croczi.teami.models
+
+import android.os.Parcel
+import android.os.Parcelable
+
+class StatusResource(var id:Int, var text:String) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString()
+    )
+    override fun toString(): String {
+        return text
+    }
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeString(text)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<StatusResource> {
+        override fun createFromParcel(parcel: Parcel): StatusResource {
+            return StatusResource(parcel)
+        }
+
+        override fun newArray(size: Int): Array<StatusResource?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
